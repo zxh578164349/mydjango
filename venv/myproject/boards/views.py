578df2652e97272a9dd172998cad88b9f5d60f68
zxh578164349@qq.com <zxh578164349@qq.com>
2018-04-17 16:59:45 +0800

    20180417

---
+++ venv/myproject/boards/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from boards.models import Board
+
+# Create your views here.
+
+def home(request):
+	boards=Board.objects.all()
+	return render(request,'home.html',{'boards':boards})
+
+def board_topics(request,pk):
+	board=get_object_or_404(Board,pk=pk)
+	return render(request,'topics.html',{'board':board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
